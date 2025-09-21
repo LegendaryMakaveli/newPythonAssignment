d7ef1d402d83ad14e94f30e989ae3b076eac14dd
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-09-21 02:17:50 +0100

    done

---
+++ chapter_six/set_manipulation.py
@@ -0,0 +1,12 @@
+set1 = {'red', 'green', 'blue'}
+set2 = {'cyan', 'green', 'blue', 'magenta', 'red'}
+
+print(set1 < set2)
+print(set1 > set2)
+print(set1 == set2)
+
+
+print(set1 | set2)   
+print(set1 & set2)
+print(set1 - set2)
+print(set1 ^ set2)
