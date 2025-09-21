5e32a9347558922e65cff26d2d7b43fadab99598
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-09-21 02:18:54 +0100

    done

---
+++ chapter_six/translate_dict.py
@@ -0,0 +1,11 @@
+translations = {
+    "hello": "hola",
+    "world": "mundo",
+    "good": "bueno",
+    "day": "día",
+    "friend": "amigo"
+}
+
+print("English   Translation")
+for eng, trans in translations.items():
+    print(f"{eng:<10}{trans}")
